7429e878cae7bc50c41b25c750a90bbf2c159ba1
Simon Adamov <simon.adamov@meteoswiss.ch>
2023-11-06 14:25:33 +0100

    Multinode training fixed, torchrun replaced

--- test_slurm.sh
+++ test_slurm.sh
@@ -1,18 +1,16 @@
-#!/bin/bash
+#!/bin/bash -l
 #SBATCH --job-name=pl_ddp_test
-#SBATCH --output=pl_ddp_test.out
-#SBATCH --error=pl_ddp_test.err
-#SBATCH --nodes=1
-#SBATCH --gres=gpu:4
+#SBATCH --output=lightning_logs/pl_ddp_test.out
+#SBATCH --error=lightning_logs/pl_ddp_test.err
+#SBATCH --nodes=2
+#SBATCH --gpus-per-node=4
+#SBATCH --ntasks-per-node=4
 #SBATCH --time=01:00:00
-#SBATCH --partition=normal
+#SBATCH --partition=a100-80gb
 #SBATCH --account=s83
 
 # Load necessary modules
-source ${CONDA_PREFIX}/etc/profile.d/conda.sh
 conda activate neural-ddp
 
-NUM_GPUS=$(echo $SLURM_JOB_GPUS | tr ',' '\n' | wc -l)
-
 # Run the script
-srun torchrun --nnodes=$SLURM_NNODES --nproc_per_node=$NUM_GPUS test_ddp.py
+srun -ul --gpus-per-task=1 python test_ddp.py
